2963ffd208bff08fdf2cf0939ed48c4cd186d83d
samosa pride <ashutoshpawar1208@gmail.com>
2023-03-06 18:52:59 +0000

    fetch

--- backend/router/Path.js
+++ backend/router/Path.js
@@ -3,11 +3,6 @@ const connectDb = require('../middleware/mongoDB')
 const router = express.Router()
 
 router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
     connectDb();
     next()
   })
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Menu = () => {
+  useEffect(() => {
+    fetch('http/localhost:8080/samosas/')
+      .then(response => response.json())
+      .then(json => window.alert(json))
+      .catch((err) => { window.alert(err) })
+  })
+
   return (
     <div>Menu</div>
   )
