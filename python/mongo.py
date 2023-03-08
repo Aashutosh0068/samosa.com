9249791b4ab2833dc4523923d0ed4d9e82b56678
samosa pride <ashutoshpawar1208@gmail.com>
2023-03-08 04:17:43 +0000

    next

--- python/mongo.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-def connectDb():
-    client = pymongo.MongoClient("mongodb+srv://Samosas:eatpeRepeat69@cluster0.7a0t84a.mongodb.net/?retryWrites=true&w=majority")
-    db = client['samosa']
-    col = db['samosas']
-    x = col.find({})
-    return list(x)
