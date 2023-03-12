5b036983b8a542b682956ec2d855f54ff7059581
samosa pride <ashutoshpawar1208@gmail.com>
2023-03-12 17:46:30 +0000

    login api

---
+++ samosa/pages/api/loginuser.js
@@ -0,0 +1,39 @@
+import userCredentials from "@/models/userCredentials"
+import cryptoJs from "crypto-js"
+import mongoose from "mongoose"
+
+export default async function handler(req, res) {
+    const FormData = req.body
+    var METHOD = req.method
+
+    if (METHOD === "POST") {
+        if (FormData.email && FormData.password) {
+            if (mongoose.connections[0].readyState) {
+                await mongoose.connect(process.env.MONGO_URI)
+            }
+
+            const user_Validation = await userCredentials.findOne({ email: FormData.email })
+
+            if (!user_Validation) {
+                res.status(690).send({ "message": "invalid email address" })
+            }
+            else {
+                if (cryptoJs.AES.decrypt(user_Validation.password, process.env.SECRET_KEY).toString() == FormData.password) {
+                    res.status(200).send({ "message": "authentication sucessfull" })
+                }
+                else {
+                    res.status(699).send({ "message": "bad credentials" })
+                    cryptoJs.AES.decrypt(user_Validation.password)
+                }
+            }
+        }
+        else {
+            res.status(500).send({ "message": "invalid body" })
+        }
+    }
+
+    else {
+        res.status(500).send({ 'message': 'method not allowed' })
+    }
+
+}
