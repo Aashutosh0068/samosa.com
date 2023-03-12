dc06dea1549b9da7c478c7468504bf0d18a80b14
samosa pride <ashutoshpawar1208@gmail.com>
2023-03-12 17:54:00 +0000

    err

--- samosa/pages/api/loginuser.js
+++ samosa/pages/api/loginuser.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
                 }
 
             }
-            catch { res.status(800).send({ 'message': err }) }
+            catch { res.status(800).send({ 'message': "err" }) }
         }
         else {
             res.status(500).send({ "message": "invalid body" })
