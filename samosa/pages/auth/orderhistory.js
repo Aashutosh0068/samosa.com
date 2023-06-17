4757105c6939853e6b26d84fc609a43644341477
Aashutosh Pawar <aashutoshpawar0068@outlook.com>
2023-06-17 20:21:13 +0530

    Update orderhistory.js

--- samosa/pages/auth/orderhistory.js
+++ samosa/pages/auth/orderhistory.js
@@ -8,19 +8,30 @@ const orderhistory = ({ data }) => {
     <div>
       {data ?(
       <>
-          <div>
-    <main class="container mx-auto py-6"/>
+          <main class="container mx-auto py-6">
   <h1 class="text-3xl font-bold mb-4">Order History</h1>
   
-    {data.map(orders => (   
-    <div class="relative bg-white shadow-md rounded-lg overflow-hidden">
-      <img src="/alt.jpg" alt="Order 2" class="w-full h-48 object-cover object-center"/>
-      <div class="absolute top-2 right-2 flex items-center">
-        <span class="bg-yellow-500 text-white px-2 py-1 rounded-full text-sm">In Progress</span>
-        <i class="fas fa-spinner text-yellow-500 text-2xl ml-2"></i>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {data.map(order =>(
+    <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <div class="relative">
+        <img src="path/to/order1.jpg" alt="Order 1" class="w-full h-64 object-cover object-center">
+        <div class="absolute top-0 right-0 bg-blue-500 text-white px-2 py-1 rounded-bl-lg">
+          <span class="text-xs font-bold">Delivered</span>
+        </div>
+        <div class="absolute bottom-0 left-0 w-full bg-gradient-to-t from-gray-900 to-transparent px-4 py-3">
+          <h2 class="text-2xl font-bold text-white mb-2">Order #12345</h2>
+          <p class="text-white">Items: Samosa (x3), Chutney</p>
+          <p class="text-white">Total: $12.50</p>
+        </div>
       </div>
-  </div>))}
+      <div class="p-4">
+        <a href="#" class="text-blue-500 hover:underline inline-block">View Details</a>
+      </div>
+    </div>
+    ))}
   </div>
+</main>
       </>
       ):(<></>)}
     </div>
